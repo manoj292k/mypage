dac33fc8847d98c083decf1f87c96c949e17b3d6
manoj292k <143692735+manoj292k@users.noreply.github.com>
2023-12-23 11:46:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
 function clickMenu() {
     var box = document.querySelector("nav");
     box.classList.toggle("nav");
-    box.classList.toggle("blue");
+    var menuIcon = document.querySelector(".menu-bar");
+
+    // Check if the element is found
+    if (menuIcon) {
+        // Toggle the class "menu-bar-rotated" on the element
+        menuIcon.classList.toggle("menu-bar-rotated");
+        console.log("rotate working");
+    } else {
+        console.error("Element with class 'menu-bar' not found.");
+    }
+
+    
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var loader = document.getElementById("preloader");
+
+    if (loader) {
+        window.addEventListener("load", function() {
+            loader.style.display = "none";
+            console.log("preloader working");
+        });
+    } else {
+        console.error("Element with ID 'preloader' not found.");
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var fadeElements = document.querySelectorAll(".fade-in");
+  
+    function checkFade() {
+      fadeElements.forEach(function (element) {
+        if (isElementInViewport(element)) {
+          element.classList.add("fade-in-visible");
+        }
+      });
+    }
+  
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+
+    checkFade();
+
+
+    window.addEventListener("scroll", checkFade);
+  });
